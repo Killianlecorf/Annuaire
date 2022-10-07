cf38f8e0f152b35772ce2e9e926bfc7f2a693d4b
killian-lecorf <killianlecorf@outlook.fr>
2022-10-07 14:00:59 +0200

    bug form

--- templates/form.php
+++ templates/form.php
@@ -5,22 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../Assets/css/form.scss">
+    <link rel="stylesheet" href="../Assets/css/form.css">
 </head>
 
 <section class="form-part">
     <div class="form-content">
+        <h1>Ajouter</h1>
         <form id="form-add" action="">
             <label for="">Nom</label>
-            <input type="text">
+            <input type="text" required>
             <label for="">prenom</label>
-            <input type="text">
+            <input type="text" required>
             <label for="">E-mail</label>
-            <input type="text">
+            <input type="text" required>
             <label for="">Numéro</label>
-            <input type="text">
+            <input type="text" required>
             <label for="">Spécialité</label>
-            <input type="text">
+            <input type="text" required>
+            <input type="submit">
         </form>
     </div>
 </section>
