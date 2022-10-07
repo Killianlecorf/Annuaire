7e7b3961f0723a462c147b174520df9bcc8eadeb
killian-lecorf <killianlecorf@outlook.fr>
2022-10-07 09:57:41 +0200

    front form

---
+++ templates/form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../Assets/css/form.scss">
+</head>
+
+<section class="form-part">
+    <div class="form-content">
+        <form id="form-add" action="">
+            <label for="">Nom</label>
+            <input type="text">
+            <label for="">prenom</label>
+            <input type="text">
+            <label for="">E-mail</label>
+            <input type="text">
+            <label for="">Numéro</label>
+            <input type="text">
+            <label for="">Spécialité</label>
+            <input type="text">
+        </form>
+    </div>
+</section>
