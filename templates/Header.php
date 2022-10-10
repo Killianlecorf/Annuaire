14d65add2a574c5fced2e99ff737ae2395fa2213
killian-lecorf <killianlecorf@outlook.fr>
2022-10-10 13:55:31 +0200

    structure front

--- templates/Header.php
+++ templates/Header.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../Assets/css/Header.scss">
-</head>
 <header>
     <section class="header-part">
         <div class="header-content">
--- templates/form.php
+++ templates/form.php
@@ -1,27 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../Assets/css/form.css">
-</head>
-
 <section class="form-part">
     <div class="form-content">
         <h1>Ajouter</h1>
         <form id="form-add" action="">
             <label for="">Nom</label>
-            <input type="text" required>
+            <input name="user_name" type="text" required>
             <label for="">prenom</label>
-            <input type="text" required>
+            <input name="user_lname" type="text" required>
             <label for="">E-mail</label>
-            <input type="text" required>
+            <input name="user_email" type="text" required>
             <label for="">Numéro</label>
-            <input type="text" required>
+            <input name="user_number" type="text" required>
             <label for="">Spécialité</label>
-            <input type="text" required>
+            <input name="user_spe" type="text" required>
             <input id="submit-button" type="submit">
         </form>
     </div>
