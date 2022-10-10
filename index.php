1e8a26c6a98c81fcecf80f93e9050459dbc5209d
killian-lecorf <killianlecorf@outlook.fr>
2022-10-10 14:03:54 +0200

    structure html

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../Assets/css/form.css">
+</head>
+<body>
+    <?php
 
-include('../Annuaire/config/ConnectDB.php');
+    include('../Annuaire/config/ConnectDB.php');
 
-$mysqlconnection = Database::BDD() ;
+    // $mysqlconnection = Database::BDD() ;
 
-include('../Annuaire/templates/Header.php');
+    include('../Annuaire/templates/Header.php');
+
+    include('../Annuaire/templates/form.php')
+    ?>
+</body>
+</html>
+
 
