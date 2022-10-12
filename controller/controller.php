20dd1b9e7586d9d5c368ed2db9c2f5cc505c0fa6
killian-lecorf <killianlecorf@outlook.fr>
2022-10-12 09:56:03 +0200

    insert bdd

--- controller/controller.php
+++ controller/controller.php
@@ -1,25 +1,27 @@
 <?php
 
 require_once __DIR__ . '/../config/ConnectDB.php';
+require_once __DIR__ . '/../models/MessageManager.php';
 
+class BDDupdate{
+    
+    public static function verification() {
+        
 
-function insert() {
-    $mysqlconnection = database::BDD();
-    $name = $_POST['user_name'];
-    $lname = $_POST['user_lname'];
-    $email = $_POST['user_email'];
-    $number = $_POST['user_number'];
-    $spe = $_POST['user_spe'];
+        if (isset($_POST['user_name'])!= "" && 
+        isset($_POST['user_lname'])!= "" &&
+        isset($_POST['user_email'])!= "" && 
+        isset($_POST['user_number'])!= "" && 
+        isset($_POST['user_name'])!= ""  ) {
 
-    if (isset($_POST['user_name'])!= "" && 
-    isset($_POST['user_name'])!= "" &&
-    isset($_POST['user_name'])!= "" && 
-    isset($_POST['user_name'])!= "" && 
-    isset($_POST['user_name'])!= ""  ) {
-        $request = "INSERT INTO `user`(`user_name`,`user_lname`,`user_mail`,`user_numero`,`user_spe`) VALUES ('$name' , '$lname','$email' ,'$number' , '$spe');";
-        $mysqlconnection->exec($request);
+            $name =$_POST['user_name'];
+
+            MessageManager::insert($name);
+
+        }
     }
-    var_dump($name);
+
 }
 
-insert();
+BDDupdate::verification();
+
--- models/MessageManager.php
+++ models/MessageManager.php
@@ -1,8 +1,17 @@
 <?php
 
-// class  BDDupdate{
-//     function insert($mysqlconnection) {
-//         $request = "INSERT INTO `user`(`user_name`,`user_lname`,`user_mail`,`user_numero`,`user_spe`) VALUES ('$name' , '$lname','$email' ,'$number' , '$spe');";
-//         $mysqlconnection->exec($request);
-//         }
-// }
+require_once __DIR__ . '/../config/ConnectDB.php';
+require_once __DIR__ . '/../controller/controller.php';
+
+class  MessageManager{
+
+    public static function insert($name) {
+
+        $mysqlconnection = database::BDD();
+        $numero = 35952;
+        $request = "INSERT INTO `user`(`user_name`,`user_lname`,`user_mail`,`user_numero`,`user_spe`) VALUE ('$name' , '$name','$name' ,'$numero' , '$name');";
+        $mysqlconnection->exec($request);
+
+        header("Location: ../models/MessageManager.php");
+    }
+}
