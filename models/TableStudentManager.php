b940d57471f1f6ffa1536ece3cb9810010092478
killian-lecorf <killianlecorf@outlook.fr>
2022-10-13 09:11:45 +0200

    View table

--- models/TableStudentManager.php
+++ models/TableStudentManager.php
@@ -6,7 +6,17 @@ class TableStudentManager{
 
     public static function ViewTable() {
         $mysqlconnection = database::BDD();
-        // $request =
+        $sql = "SELECT * FROM `user`";
+        $selectmysql =  $mysqlconnection->query($sql);
+        while($row = $selectmysql->fetch(PDO::FETCH_ASSOC)) : ?>
+            <tr>
+                <td><?php echo htmlspecialchars($row['user_name']); ?></td>
+                <td><?php echo htmlspecialchars($row['user_lname']); ?></td>
+                <td><?php echo htmlspecialchars($row['user_mail']); ?></td>
+                <td><?php echo htmlspecialchars($row['user_numero']); ?></td>
+                <td><?php echo htmlspecialchars($row['user_spe']); ?></td>
+            </tr>
+        <?php endwhile; 
     }
 
 
