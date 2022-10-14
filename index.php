8881ddd901874bf7f001c9bcf0be6a1acca8e704
killian-lecorf <killianlecorf@outlook.fr>
2022-10-14 15:56:34 +0200

    fix

--- index.php
+++ index.php
@@ -5,19 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../Assets/css/form.css">
+    <link rel="stylesheet" href="../Annuaire/Assets/css/form.css">
+    <link rel="stylesheet" href="../Annuaire/Assets/css/readTable.css">
+    <script src="https://kit.fontawesome.com/e1106f8104.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <?php
+        include('../Annuaire/config/ConnectDB.php');
 
-    include('../Annuaire/config/ConnectDB.php');
+        include('../Annuaire/templates/form.php');
 
-    // $mysqlconnection = Database::BDD() ;
+    ?>
+    <button onclick="displayBlockForm()">ouvrir</button>
 
-    include('../Annuaire/templates/Header.php');
+    <section class="sectionTable">
+            <?php
+                include('../Annuaire/templates/readTable.php')
+            ?>
+    </section>
 
     <script src="../Annuaire/Assets/js/script.js"></script>
 </body>
 </html>
-
-
