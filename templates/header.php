3469de0d759c59461c526e92145bf80e0a275f01
killian-lecorf <killianlecorf@outlook.fr>
2022-10-28 17:57:55 +0200

    add search

--- templates/header.php
+++ templates/header.php
@@ -6,7 +6,19 @@
             </div>
         </div>
         <div class="nav-content">
-                <button onclick="displayBlockForm()" id="button-insert">Ajouter</button>
-            </div>
+            <button onclick="displayBlockForm()" id="button-insert">Ajouter</button>
+            <button onclick="displayBlockForm()" id="button-insert2">Ajouter</button>
+        </div>
+    </section>
+    <section class="filter-part">
+        <div class="filter-content">
+
+        </div>
+        <div class="search-content">
+            <form action="" method="GET">
+                <input class="search-header" type="search" name="q" placeholder="Rechercher un étudiant">
+                <input class="submit-header" type="submit" value="Rechercher">
+            </form>
+        </div>
     </section>
 </header>
